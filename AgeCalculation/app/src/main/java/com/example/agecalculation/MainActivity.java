ae52b88f35ae508eab12c2c4191b2ecb2f038b01
asmaa khaled <asmaakhaled603@gmail.com>
2020-08-18 21:19:09 +0200

    AgeClaculation keeps stopping Error!!!!

---
+++ AgeCalculation/app/src/main/java/com/example/agecalculation/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.agecalculation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+     EditText edtYear;
+     TextView txtAge;
+     Button btnResult;
+     int finalResult , birthYear;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edtYear=(EditText)findViewById(R.id.editText);
+        txtAge = (TextView)findViewById(R.id.textView3);
+        btnResult=(Button)findViewById(R.id.btnAge);
+        birthYear = Integer.parseInt(edtYear.getText().toString());
+
+        btnResult.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finalResult= 2020-birthYear;
+                txtAge.setText(" "+finalResult);
+            }
+        });
+
+    }
+}
